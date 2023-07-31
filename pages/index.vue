412fe707714fd3075f48ca51002c2c4b3feac02b
danish2016 <danish.danish.danish2016@gmail.com>
2023-07-31 22:21:52 +0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <h1 class="text-8xl">Find24</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped></style>
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fontFamily: {},
+    extend: {
+      screens: {},
+      colors: {},
+      spacing: {},
+      borderRadius: {},
+    },
+  },
+};
