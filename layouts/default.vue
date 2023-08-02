53ad97fae314f3b10706eb3916cd4e06bfba6e2c
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-02 17:03:02 +0500

    Added design for footer

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="mx-auto max-w-120 text-dark-jungle-green">
+    <slot />
+    <DefaultFooter />
+  </div>
+</template>
+
+<style scoped></style>
