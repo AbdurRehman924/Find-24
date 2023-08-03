15ee451518a000bea6aac95509bd2da437feb6af
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-03 11:18:41 +0500

    Added title template for whole app

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+  useHead({
+    titleTemplate: (titleChunk) => {
+      return titleChunk ? `${titleChunk} - Find24` : "Find24";
+    },
+  });
+</script>
+
 <template>
   <NuxtPage />
 </template>
