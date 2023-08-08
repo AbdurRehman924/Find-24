862ac06d0142d5d1ac71e227a797684a5bded072
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-08 14:48:07 +0500

    Added result card skeleton

---
+++ components/search/ResultCardSkeleton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="flex flex-col gap-8 rounded-2xl border-0.5 border-tonal_dark_mercury p-4 shadow-variant4"
+  >
+    <div
+      class="h-48 w-[330px] animate-pulse rounded-lg bg-corduroy bg-opacity-50"
+    />
+    <div class="flex flex-col gap-2">
+      <span
+        class="h-6 animate-pulse bg-corduroy bg-opacity-50"
+      ></span>
+      <span
+        class="h-6 w-32 animate-pulse bg-corduroy bg-opacity-50"
+      ></span>
+      <p class="flex items-center gap-1 text-base font-medium">
+        <span class="flex items-center gap-1">
+          <span
+            class="h-6 w-48 animate-pulse bg-corduroy bg-opacity-50"
+          ></span>
+        </span>
+      </p>
+      <p class="flex items-center justify-between">
+        <span
+          class="h-6 w-32 animate-pulse bg-corduroy bg-opacity-50"
+        ></span>
+        <span
+          class="h-6 w-12 animate-pulse bg-corduroy bg-opacity-50"
+        ></span>
+      </p>
+    </div>
+  </div>
+</template>
--- components/search/ResultsList.vue
+++ components/search/ResultsList.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
+  import Service from "~/types/service";
+
   const { fetchServices } = useServices();
-  const services = await fetchServices();
+  const loading = ref(true);
+
+  const services = ref<Service[]>([]);
+
+  onMounted(async () => {
+    services.value = await fetchServices();
+    loading.value = false;
+  });
 </script>
 
 <template>
-  <div class="flex flex-wrap items-center justify-center gap-6">
-    <ClientOnly>
-      <div v-for="service in services" :key="service.id">
-        <SearchResultCard :service="service" />
-      </div>
-    </ClientOnly>
+  <div
+    class="flex flex-wrap items-center justify-center gap-6"
+    v-if="!loading"
+  >
+    <div v-for="service in services" :key="service.id">
+      <SearchResultCard :service="service" />
+    </div>
+  </div>
+
+  <div
+    v-else
+    class="flex flex-wrap items-center justify-center gap-6"
+  >
+    <div v-for="i in 10"><SearchResultCardSkeleton /></div>
   </div>
 </template>
 
