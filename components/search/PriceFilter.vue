21126fa4f02653f3736d0bbfbc7ac37b185695cb
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-08-11 14:02:34 +0500

    Added design for price range picker

--- components/search/PriceFilter.vue
+++ components/search/PriceFilter.vue
@@ -5,6 +5,29 @@
   const servicesStore = useServicesStore();
 
   const { minPrice, maxPrice } = storeToRefs(servicesStore);
+
+  const maximumPriceGap = ref(100);
+  const leftPercent = ref((minPrice.value / maxPrice.value) * 100);
+  const rightPercent = ref(0);
+  const minInput = ref<HTMLInputElement>();
+  const min = ref(minPrice.value);
+  const max = ref(maxPrice.value);
+
+  function handleRangeInput(event: Event) {
+    if (min.value < minPrice.value) {
+      min.value = minPrice.value;
+    }
+    if (max.value - min.value < maximumPriceGap.value) {
+      if (event.target == minInput.value) {
+        min.value = max.value - maximumPriceGap.value;
+      } else {
+        max.value = min.value + maximumPriceGap.value;
+      }
+    } else {
+      leftPercent.value = (min.value / maxPrice.value) * 100;
+      rightPercent.value = 100 - (max.value / maxPrice.value) * 100;
+    }
+  }
 </script>
 
 <template>
@@ -13,9 +36,37 @@
       Price Range
     </h4>
     <div>
-      <div class="h-1 rounded-2xl bg-chinese-white"></div>
+      <div class="relative h-1 rounded-2xl bg-chinese-white">
+        <div
+          class="absolute h-full rounded-2xl bg-palma"
+          :style="`left: ${leftPercent}%; right: ${rightPercent}%`"
+        ></div>
+      </div>
+      <div class="range-input">
+        <label for="min-price" class="sr-only">Minimum price</label>
+        <input
+          type="range"
+          class="range-min"
+          name="min-price"
+          :min="0"
+          :max="maxPrice"
+          v-model="min"
+          ref="minInput"
+          @input="handleRangeInput"
+        />
+        <label for="max-price" class="sr-only">Maximum price</label>
+        <input
+          type="range"
+          class="range-max"
+          name="max-price"
+          :min="0"
+          :max="maxPrice"
+          v-model="max"
+          @input="handleRangeInput"
+        />
+      </div>
       <div class="mt-5 flex justify-between">
-        <span>$ {{ minPrice }}</span> <span>$ {{ maxPrice }}</span>
+        <span>$ 0</span> <span>$ {{ maxPrice }}</span>
       </div>
     </div>
   </div>
