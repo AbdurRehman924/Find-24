6a924f9b8fdf87845ba1bbbf7891d964b900b082
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-09-16 12:25:16 +0500

    Added title

--- pages/provider/signup.vue
+++ pages/provider/signup.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="mx-auto max-w-1920 px-4 sm:px-8 lg:px-16">
-    Provider signup
+  <main class="mx-auto max-w-1920 px-4 py-14 sm:px-8 lg:px-16">
+    <div class="text-center">
+      <h1
+        class="mb-4 text-2xl font-bold sm:text-3xl lg:text-4xl xl:text-5xl"
+      >
+        Empower Your Earning Journey!
+      </h1>
+      <p class="text-dark_corduroy">
+        Join us free to showcase your skills and connect with a vast
+        customer base
+      </p>
+    </div>
   </main>
 </template>
 
